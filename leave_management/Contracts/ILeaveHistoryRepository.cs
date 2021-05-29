dd96dc5d00fe9f81736670efbd438422f5fe6a23
USUARIO <USUARIO@DESKTOP-A1J243G>
2021-05-29 17:30:42 -0500

    Add project files.

---
+++ leave_management/Contracts/ILeaveHistoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface ILeaveHistoryRepository : IRepositoryBase<Data.LeaveHistory>
+    {
+    }
+}
