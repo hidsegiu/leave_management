39beda84a008bb46d1013160522ab615f11bf19d
GERSON SAMIR GARCIA SOTOMAYOR <A19200857@idat.edu.pe>
2021-05-29 20:04:29 -0500

    Updated

--- leave_management/Contracts/ILeaveHistoryRepository.cs
+++ leave_management/Contracts/ILeaveHistoryRepository.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using leave_management.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace leave_management.Contracts
 {
-    public interface ILeaveHistoryRepository : IRepositoryBase<Data.LeaveHistory>
+    public interface ILeaveHistoryRepository : IRepositoryBase<LeaveHistory>
     {
     }
 }
--- leave_management/Contracts/ILeaveTypeRepository.cs
+++ leave_management/Contracts/ILeaveTypeRepository.cs
@@ -1,5 +1,4 @@
 ﻿using leave_management.Data;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,5 +7,5 @@ namespace leave_management.Contracts
 {
     public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
     {
-     }
+    }
 }
